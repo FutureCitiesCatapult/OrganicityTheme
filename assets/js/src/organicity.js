a07be250119a45315f631b1f7ed76863437f6988
Tom Hartnell <thartnell@futurecities.catapult.org.uk>
2015-05-05 17:01:06 +0100

    initial import

---
+++ assets/js/src/organicity.js
@@ -0,0 +1,13 @@
+/**
+ * Organicity
+ * http://www.organicity.eu
+ *
+ * Copyright (c) 2015 Tom Hartnell
+ * Licensed under the GPLv2+ license.
+ */
+ 
+ ( function( window, undefined ) {
+	'use strict';
+
+
+ } )( this );
---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * @package Organicity
+ * @since 0.1.0
+ */
