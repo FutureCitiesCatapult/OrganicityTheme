56204b5adf74c253176eff82416e92d512c80558
Rebecca Jones <rebeccajones@FCCMAC6043.local>
2015-05-19 11:37:56 +0100

    Styled the footer.

--- organicity/footer.php
+++ organicity/footer.php
@@ -8,26 +8,36 @@
 ?>
 
 <footer>
+    <div class="section section--footer">
   <div class="pure-g">
     <div class="pure-u-1-4"></div>
     <div class="pure-u-1-2">
 
-      <h2>Follow us</h2>
+      <h3>Follow us</h3>
 
         <div class="socialMedia">
-            <a href=""><i class="icon-facebook-squared"></i></a>
-            <a href=""><i class="icon-twitter"></i></a>
-            <a href=""><i class="icon-slideshare"></i></a>
-            <a href=""><i class="icon-linkedin-squared"></i></a>
-            <a href=""><i class="icon-instagramm"></i></a>
-            <a href=""><i class="icon-youtube-play"></i></a>
+
+            <a href="<?php echo get_option('organicity_url_facebook'); ?>"><i class="icon-facebook-squared"></i></a>
+            <a href="<?php echo get_option('organicity_url_twitter'); ?>"><i class="icon-twitter"></i></a>
+            <a href="<?php echo get_option('organicity_url_slideshare'); ?>"><i class="icon-slideshare"></i></a>
+            <a href="<?php echo get_option('organicity_url_linkedin'); ?>"><i class="icon-linkedin-squared"></i></a>
+            <a href="<?php echo get_option('organicity_url_instagram'); ?>"><i class="icon-instagramm"></i></a>
+            <a href="<?php echo get_option('organicity_url_youtube'); ?>"><i class="icon-youtube-play"></i></a>
         </div>
 
         <hr/>
+        <div class="logos">
+            <img
+                class=""
+                src="<?php bloginfo('template_directory'); ?>/images/logo_eu.png"
+                alt="<?php _e('European Commission', 'organicity'); ?>"
+                />
+        </div>
 
     </div>
     <div class="pure-u-1-4"></div>
   </div>
+    </div>
   <div class="section section--copyright">
     <div class="pure-g">
       <div class="pure-u-1-4"></div>
