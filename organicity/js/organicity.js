3eacfe449ecc2c31f4d3ef6e1a8bcddabf4a3f45
Rebecca Jones <rebeccajones@FCCMAC6043.local>
2015-06-17 13:38:44 +0100

    Header hompage styling complete

--- organicity/js/organicity.js
+++ organicity/js/organicity.js
@@ -1,4 +1,4 @@
-/*! Organicity - v0.1.0 - 2015-06-12
+/*! Organicity - v0.1.0 - 2015-06-17
  * http://www.organicity.eu
  * Copyright (c) 2015 Future Cities Catapult */
 // ( function( window, undefined ) {
