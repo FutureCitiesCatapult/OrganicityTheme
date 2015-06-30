887968e703d924c21ad39b339e3ac008e5967696
Rebecca Jones <rebeccajones@FCCMAC6043.local>
2015-06-30 18:37:31 +0100

    Refactored the sass into modules so its easier to edit. Sorted out padding across the site. Front page styling for fonts across all sizes. Signup button box fixed on tablet and desktop

--- organicity/front-page.php
+++ organicity/front-page.php
@@ -148,7 +148,7 @@ get_header(); ?>
   <div class="section section--findevents">
     <div class="pure-g">
       <div class="pure-u-1-4"></div>
-      <div class="pure-u-1-2">
+      <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
         <h3><?php echo rwmb_meta('organicity_homepage_events_section_title'); ?></h3>
         <?php echo rwmb_meta('organicity_homepage_events_section_content'); ?>
         <a class="button button--full button--external" href="<?php echo get_post_type_archive_link('event'); ?>">
@@ -164,7 +164,7 @@ get_header(); ?>
   <div class="section section--signup">
     <div class="pure-g">
       <div class="pure-u-1-4"></div>
-      <div class="pure-u-1-2">
+      <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
         <h3><?php echo rwmb_meta('organicity_homepage_signup_section_title'); ?></h3>
         <?php echo rwmb_meta('organicity_homepage_signup_section_content'); ?>
         <form class="pure-form pure-g">
--- organicity/single-event.php
+++ organicity/single-event.php
@@ -62,16 +62,19 @@ get_header(); ?>
             <div class="section section--signup">
                 <div class="pure-g">
                     <div class="pure-u-1-4"></div>
-                    <div class="pure-u-1-2">
+                    <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
                         <h3><?php echo rwmb_meta('organicity_homepage_signup_section_title',[],$frontpage_id); ?></h3>
                         <?php echo rwmb_meta('organicity_homepage_signup_section_content',[],$frontpage_id); ?>
-                        <form class="pure-form">
-                            <fieldset>
+                        <form class="pure-form pure-g">
+                            <div class="pure-u-2-3 pure-u-md-3-4">
                                 <input class="" type="email" placeholder="<?php echo rwmb_meta('organicity_homepage_signup_field_placeholder_text',[],$frontpage_id); ?>">
+                                </div>
+                            <div class="pure-u-1-3 pure-u-md-1-4">
                                 <button type="submit" class="button button--bordered button--internal">
                                     <?php echo rwmb_meta('organicity_homepage_signup_button_text',[],$frontpage_id); ?>
                                 </button>
-                            </fieldset>
+                                </div>
+
                         </form>
                     </div>
                     <div class="pure-u-1-4"></div>
