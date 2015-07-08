e0e35469e0c591a1cfb20a3c674f892a554e2068
Rebecca Jones <rebeccajones@FCCMAC6043.local>
2015-07-08 12:04:16 +0100

    Design amendments with Paul

--- organicity/partials/findevent.php
+++ organicity/partials/findevent.php
@@ -3,7 +3,7 @@
         <div class="pure-g">
             <div class="pure-u-1-4"></div>
             <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
-                <h3><?php echo rwmb_meta('organicity_homepage_events_section_title'); ?></h3>
+                <h2><?php echo rwmb_meta('organicity_homepage_events_section_title'); ?></h2>
                 <?php echo rwmb_meta('organicity_homepage_events_section_content'); ?>
                 <a class="button button--full button--external" href="<?php echo get_post_type_archive_link('event'); ?>">
                     <?php echo rwmb_meta('organicity_homepage_events_link_text'); ?>
