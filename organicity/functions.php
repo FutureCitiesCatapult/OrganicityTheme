947b756c8089e867812bb1849eb82f383b62914e
Rebecca Jones <rebeccajones@FCCMAC6043.local>
2015-08-06 18:30:33 +0100

    Resize feature blog card image and auto crop

--- organicity/functions.php
+++ organicity/functions.php
@@ -29,6 +29,8 @@ add_action( 'after_setup_theme', 'organicity_setup' );
  */
 function add_post_thumbnail_support() {
     add_theme_support('post-thumbnails');
+    set_post_thumbnail_size( 450, 450, true );
+
 }
 add_filter('init', 'add_post_thumbnail_support');
 
