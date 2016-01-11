4209712b6fc182d81cf8152ed594d808ed275ef9
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-01-11 14:15:15 +0000

    Add open-call announcement page
    So far has dummy content. I've moved the FAQs 'theme'
    to another file pending design.
    
    Rather suspicious change to _fonts to scope the 'body
    text' styles to within a `main` tag. One to watch.

--- organicity/page-open-call.php
+++ organicity/page-open-call.php
@@ -7,51 +7,76 @@
  */
 get_header(); ?>
 
-<?php
-
-$faqs_query_args = array(
-    'post_type'      => 'faq',
-    'posts_per_page' => -1
-);
-$faqs_query = new WP_Query( $faqs_query_args );
-
-?>
-
 <main role="main">
-
-    <div class="section section--blog section--wide">
-        <h2><?php _e( 'Open Call', 'organicity' ); ?></h2>
-        <h3><?php _e( 'FAQs', 'organicity' ); ?></h3>
-
-        <div class="pure-g">
-            <!-- loop to show all FAQs -->
-            <?php if ( $faqs_query->have_posts() ) : ?>
-
-            <div class="blog-large home">
-                <?php while ( $faqs_query->have_posts() ) : $faqs_query->the_post(); ?>
-                    <?php get_template_part('partials/faq'); ?>
-                <?php endwhile; ?>
-            </div>
-            <?php wp_reset_postdata(); ?>
-
-            <?php endif; ?>
-        </div>
-    </div>
-
-
-    <div id="main" class="section section--main">
+    <div class="section rich-title">
         <div class="pure-g">
             <div class="pure-u-1-4"></div>
-            <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
-                <?php the_content(); ?>
+            <div class="pure-u-1-1 pure-u-lg-1-2">
+                <h2 class="rich-title__heading">Announcement of the First Competitive Open Call for Additional Project Beneficiaries</h2>
+                <p class="rich-title__detail">The OrganiCity project currently active in the Horizon 2020 EU Framework Programme for Research and Innovation requires the participation of additional beneficiaries to conduct experiments within the project.</p>
+                <div class="pure-g button-pair">
+                    <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__left">
+                        <a class="button button--external button-pair__button">Apply now</a>
+                    </div>
+                    <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__right">
+                        <a class="button button--external button-pair__button">
+                            View instructions
+                            <span class="x-download-icon"></span>
+                        </a>
+                    </div>
+                </div>
             </div>
             <div class="pure-u-1-4"></div>
         </div>
     </div>
 
-    <?php get_template_part('partials/findevent'); ?>
+    <div class="section section--page">
+        <div class="pure-g">
+            <div class="pure-u-1-8"></div>
+            <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-6-8 section--page--content">
+                <div class="section--page--content-wrapper">
+                    <section>
+                        <h3>Become an Experimenter in OrganiCity</h3>
+                        <p>OrganiCity offers a new paradigm to European digital city making.The project seeks to develop a strong foundation for future sustainable cities through co-creation by a wide range of stakeholders. Globally, Europe is a champion of sustainable, inclusive and open societies. The digital age enables us to push this position further and to rethink the way we create cities and facilitate living by integrating many complex systems.</p>
+                        <p>OrganiCity combines top-down planning and operations with flexible bottom-up initiatives where citizen involvement is key. By focusing on the city as a socio-technical whole, OrganiCity brings software, hardware and associated human processes flexibly together into a new living city that is replicable, scalable as well as socially, environmentally and economically sustainable.</p>
+                    </section>
+                    <section>
+                        <h3>Objectives of the First Open Call January 2016</h3>
+                        <p>As civic participation and co­-creation are considered by the project as an equally important component of future cities as technology solutions, €1.8m, one quarter of the entire budget for the OrganiCity project, is reserved directly for citizen-­driven experiments. This, the first of two open calls, intends to initiate experimentation on the facility which is under development and one third of the Open Call budget is released. This means that €600.000 are available across Aarhus (DK), London (UK) and Santander (ES) allowing up to €200.000 per cluster on various experiments.</p>
+                        <p>OrganiCity is seeking innovative experiment proposals that explore the potential of the OrganiCity facility to be a greenhouse for European growth and digital urban systems development. It is the goal of this open goal to “kickstart” a new generation of city makers who will utilise, evaluate and contribute to the facility during experimentation and beyond.</p>
+                    </section>
+                    <section>
+                        <h3>City Challenges Aarhus</h3>
+                        <p><span class="x-red">The extension of the living room</span></p>
+                        <ul>
+                            <li>Many inhabitants in the inner city live in small apartments and use the city as a second living room. The citizens emphasizes the importance of life around them in the city, but at the same time they underline the importance of privacy in the public sphere.</li>
+                            <li>The smaller squares and marketplaces around the city should be taken into use and thereby create more life.</li>
+                        </ul>
+                        <p><span class="x-red">Green lungs</span></p>
+                        <ul>
+                            <li>The citizens in Aarhus have a strong wish for even more green spaces in their city.</li>
+                        </ul>
+                    </section>
+                    <div class="open-call-cta">
+                        <a class="button button--external button--full">Apply now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="pure-u-1-8"></div>
+        </div>
+    </div>
 
-    <?php get_template_part('partials/signup'); ?>
-
+    <div class="section section--page">
+        <div class="pure-g">
+            <div class="pure-u-1-8"></div>
+            <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-6-8 section--page--content">
+                <div class="section--page--content-wrapper">
+                    <h3>Need a helping hand?</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <a>Contact the helpdesk</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </main>
 <?php get_footer(); ?>
