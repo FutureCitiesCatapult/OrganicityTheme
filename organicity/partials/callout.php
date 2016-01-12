12763ca1031479f8b5ba2a46486d3308c565e397
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-01-12 14:45:33 +0000

    Remove `id=main` from callout section

--- organicity/partials/callout.php
+++ organicity/partials/callout.php
@@ -1,4 +1,4 @@
-<div id="main" class="section callout">
+<div class="section callout">
     <div class="pure-g">
         <div class="pure-u-1-4"></div>
         <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
