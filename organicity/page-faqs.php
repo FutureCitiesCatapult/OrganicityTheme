3e444a8ddc7c0eea0adbce1da891007e6a1bff84
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-01-13 15:14:37 +0000

    Remove page display in FAQs and move #main

--- organicity/page-faqs.php
+++ organicity/page-faqs.php
@@ -17,7 +17,7 @@ $cargs = array(
     );
 ?>
 
-<main role="main">
+<main role="main" id="main">
 
     <div class="section section--title">
         <div class="pure-g">
@@ -64,16 +64,6 @@ $cargs = array(
     </div>
 
     <?php wp_reset_query(); ?>
-
-    <div id="main" class="section section--main">
-        <div class="pure-g">
-            <div class="pure-u-1-4"></div>
-            <div class="pure-u-1-1 pure-u-md-1-1 pure-u-lg-1-2">
-                <?php the_content(); ?>
-            </div>
-            <div class="pure-u-1-4"></div>
-        </div>
-    </div>
 
     <?php get_template_part('partials/findevent'); ?>
 
