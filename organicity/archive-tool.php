02ee06b81cc0b49ee9bc3c45f643a81184f49756
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-01-19 10:50:47 +0000

    Make tools listing page responsive

--- organicity/archive-tool.php
+++ organicity/archive-tool.php
@@ -18,7 +18,7 @@ $posts_query = new WP_Query( $query_args );
 ?>
 
 <main role="main">
-    <div class="section section--title">
+    <div class="section section--title section--title--tools-page">
         <div class="pure-g">
             <div class="pure-u-1-4"></div>
             <div class="pure-u-1-2">
@@ -28,11 +28,11 @@ $posts_query = new WP_Query( $query_args );
         </div>
     </div>
 
-    <hr />
+    <hr class="hidden-sm" />
 
     <div class="section tools-list">
         <?php if ($posts_query->have_posts()): while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
-            <a href="#" class="tools-list__tool">
+            <a href="#tool_<?php the_id() ?>" class="tools-list__tool">
                 <div class="tools-list__title"><?= rwmb_meta('organicity_tool_label') ?></div>
                 <div class="tools-list__graphic-wrapper">
                     <img class="tools-list__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_small_outline')) ?>" />
@@ -42,13 +42,14 @@ $posts_query = new WP_Query( $query_args );
         <?php endwhile; endif; ?>
     </div>
 
-    <!-- this classless div is here to let us track nth-child -->
-    <div>
-        <?php if ($posts_query->have_posts()) : ?>
+
+    <?php if ($posts_query->have_posts()) : ?>
+        <!-- this classless div is here to let us track nth-child -->
+        <div>
             <?php while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
                 <?php if (!rwmb_meta('organicity_tool_is_full_width')) { continue; } ?>
                 <div class="finished-tool__background">
-                    <div class="section finished-tool">
+                    <div id="tool_<?php the_id() ?>" class="section finished-tool">
                         <div class="finished-tool__graphic-wrapper">
                             <img class="finished-tool__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_big')) ?>" alt="" />
                         </div>
@@ -58,14 +59,14 @@ $posts_query = new WP_Query( $query_args );
                         </div>
                         <div class="finished-tool__actions button-pair">
                             <div class="pure-g">
-                                <div class="button-pair__left pure-u-1-2">
+                                <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__left">
                                     <a href="<?= rwmb_meta('organicity_tool_link_href') ?>" class="button button-pair__button button--external" target="_blank">
                                         <span class="icon-external"></span>
                                         Open tool
                                         <span class="offscreen">(opens in new tab)</span>
                                     </a>
                                 </div>
-                                <div class="button-pair__right pure-u-1-2">
+                                <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__right">
                                     <a href="<?php the_permalink() ?>" class="button button-pair__button button--external">View details</a>
                                 </div>
                             </div>
@@ -73,36 +74,41 @@ $posts_query = new WP_Query( $query_args );
                     </div>
                 </div>
             <?php endwhile; ?>
+        </div>
 
-            <div class="tools-page__coming-soon">
-                <h2 class="tools-page__coming-soon-title">
-                    <?php _e( 'Coming soon', 'organicity' ); ?>
-                </h3>
-                <p class="tools-page__coming-soon-body">
-                    Somehow this content must be made editable. Somehow this content must be made editable. Somehow this content must be made editable.
-                </p>
-                <hr>
-            </div>
+        <div class="tools-page__coming-soon">
+            <h2 class="tools-page__coming-soon-title">
+                <?php _e( 'Coming soon', 'organicity' ); ?>
+            </h3>
+            <p class="tools-page__coming-soon-body">
+                Somehow this content must be made editable. Somehow this content must be made editable. Somehow this content must be made editable.
+            </p>
+            <hr>
+        </div>
 
-            <div class="tools-page__previews">
+        <div class="tools-page__previews">
+            <!-- this classless div is here to let us track nth-child -->
+            <div>
                 <?php while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
                     <?php if (rwmb_meta('organicity_tool_is_full_width')) { continue; } ?>
-                    <div class="preview-tool">
-                        <div class="preview-tool__graphic-wrapper">
-                            <img class="preview-tool__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_big')) ?>" alt="" />
-                        </div>
-                        <h3 class="preview-tool__heading"><?php the_title() ?></h2>
-                        <div class="preview-tool__description">
-                            <?= rwmb_meta('organicity_tool_short_description') ?>
-                        </div>
-                        <div class="preview-tool__actions">
-                            <a href="<?php the_permalink() ?>" class="button button--external button--small">Find out more</a>
+                    <div class="preview-tool__background">
+                        <div id="tool_<?php the_id() ?>" class="preview-tool">
+                            <div class="preview-tool__graphic-wrapper">
+                                <img class="preview-tool__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_big')) ?>" alt="" />
+                            </div>
+                            <h3 class="preview-tool__heading"><?php the_title() ?></h2>
+                            <div class="preview-tool__description">
+                                <?= rwmb_meta('organicity_tool_short_description') ?>
+                            </div>
+                            <div class="preview-tool__actions">
+                                <a href="<?php the_permalink() ?>" class="button button--external preview-tool__action">Find out more</a>
+                            </div>
                         </div>
                     </div>
                 <?php endwhile; ?>
             </div>
-        <?php endif; ?>
-    </div>
+        </div>
+    <?php endif; ?>
 
 
     <?php get_template_part('partials/signup'); ?>
