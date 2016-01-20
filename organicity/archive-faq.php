9893f4889b79a83bbab4fb90d88a4be3feb8dc2e
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-01-20 16:21:58 +0000

    Fix FAQ heading alignment
    Involved changing the line-height of h2 elements to
    1.1. This is unfortunately a global change but hopefully
    one we'll catch if it causes any trouble — luckily
    it's only a handful of pixels. Still.

--- organicity/archive-faq.php
+++ organicity/archive-faq.php
@@ -22,7 +22,7 @@ $cargs = array(
     <div class="section section--title">
         <div class="pure-g">
             <div class="pure-u-1-4"></div>
-            <div class="pure-u-1-2">
+            <div class="pure-u-1-1 pure-u-md-1-2">
                 <h2><?php _e( 'Frequently Asked Questions', 'organicity' ); ?></h2>
             </div>
             <div class="pure-u-1-4"></div>
