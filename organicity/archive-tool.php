9007d779763821a6f8e00202b6566351de85b023
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-02-19 14:49:45 +0000

    Move tools description above icons

--- organicity/archive-tool.php
+++ organicity/archive-tool.php
@@ -29,20 +29,6 @@ $posts_query = new WP_Query( $query_args );
         </div>
     </div>
 
-    <hr class="hidden-sm" />
-
-    <div class="section tools-list">
-        <?php if ($posts_query->have_posts()): while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
-            <a href="#tool_<?php the_id() ?>" class="tools-list__tool">
-                <div class="tools-list__title"><?= rwmb_meta('organicity_tool_label') ?></div>
-                <div class="tools-list__graphic-wrapper">
-                    <img class="tools-list__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_small_outline')) ?>" />
-                    <img class="tools-list__graphic tools-list__graphic--hover" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_small_filled')) ?>" />
-                </div>
-            </a>
-        <?php endwhile; endif; ?>
-    </div>
-
     <div class="section section--page section--tools-description">
         <div class="pure-g">
             <div class="pure-u-1-8"></div>
@@ -56,6 +42,17 @@ $posts_query = new WP_Query( $query_args );
         </div>
     </div>
 
+    <div class="section tools-list">
+        <?php if ($posts_query->have_posts()): while ($posts_query->have_posts()) : $posts_query->the_post(); ?>
+            <a href="#tool_<?php the_id() ?>" class="tools-list__tool">
+                <div class="tools-list__title"><?= rwmb_meta('organicity_tool_label') ?></div>
+                <div class="tools-list__graphic-wrapper">
+                    <img class="tools-list__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_small_outline')) ?>" />
+                    <img class="tools-list__graphic tools-list__graphic--hover" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_small_filled')) ?>" />
+                </div>
+            </a>
+        <?php endwhile; endif; ?>
+    </div>
 
     <?php if ($posts_query->have_posts()) : ?>
         <!-- this classless div is here to let us track nth-child -->
