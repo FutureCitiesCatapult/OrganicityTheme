b5418b383fe5ca7d19534d9a8db722d3fd1400c6
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-03-17 09:59:24 +0000

    Fix tools page with long description texts
    I hate floats. I hate them so dang much.

--- organicity/partials/finished-tool.php
+++ organicity/partials/finished-tool.php
@@ -3,32 +3,34 @@
         <div class="finished-tool__graphic-wrapper">
             <img class="finished-tool__graphic" src="<?= wp_get_attachment_url(rwmb_meta('organicity_tool_graphic_big')) ?>" alt="" />
         </div>
-        <h2 class="finished-tool__heading"><?php the_title() ?></h2>
-        <div class="finished-tool__description">
-            <?= rwmb_meta('organicity_tool_short_description') ?>
-        </div>
-        <div class="finished-tool__actions button-pair">
-            <div class="pure-g">
-                <?php if (rwmb_meta('organicity_tool_link_href')) { ?>
-                    <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__left">
-                        <a href="<?= rwmb_meta('organicity_tool_link_href') ?>" class="button button-pair__button button--external" target="_blank" data-internal-scroll-focus-target>
-                            <span class="icon-external"></span>
-                            Open tool
-                            <span class="offscreen">(opens in new tab)</span>
-                        </a>
-                    </div>
-                    <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__right">
-                        <a href="<?php the_permalink() ?>" class="button button-pair__button button--external">View details</a>
-                    </div>
-                <?php } else { ?>
-                    <div class="pure-u-1-1">
-                        <div class="button-pair__single">
-                            <a href="<?php the_permalink() ?>" class="button button--external button-pair__button" data-internal-scroll-focus-target>
-                                View details
+        <div class="finished-tool__detail">
+            <h2 class="finished-tool__heading"><?php the_title() ?></h2>
+            <div class="finished-tool__description">
+                <?= rwmb_meta('organicity_tool_short_description') ?>
+            </div>
+            <div class="finished-tool__actions button-pair">
+                <div class="pure-g">
+                    <?php if (rwmb_meta('organicity_tool_link_href')) { ?>
+                        <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__left">
+                            <a href="<?= rwmb_meta('organicity_tool_link_href') ?>" class="button button-pair__button button--external" target="_blank" data-internal-scroll-focus-target>
+                                <span class="icon-external"></span>
+                                Open tool
+                                <span class="offscreen">(opens in new tab)</span>
                             </a>
                         </div>
-                    </div>
-                <?php } ?>
+                        <div class="pure-u-1-1 pure-u-sm-1-2 button-pair__right">
+                            <a href="<?php the_permalink() ?>" class="button button-pair__button button--external">View details</a>
+                        </div>
+                    <?php } else { ?>
+                        <div class="pure-u-1-1">
+                            <div class="button-pair__single">
+                                <a href="<?php the_permalink() ?>" class="button button--external button-pair__button" data-internal-scroll-focus-target>
+                                    View details
+                                </a>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
             </div>
         </div>
     </div>
