d7fc9f547ad2095055f8561ba906f9a786f19d32
Kay Lovelace <klovelace@futurecities.catapult.org.uk>
2016-04-22 11:44:27 +0100

    Bump version 0.1.9 -> 0.1.10

--- organicity/functions.php
+++ organicity/functions.php
@@ -11,7 +11,7 @@
  * @package Organicity
  * @since 0.1.0
  */
-define( 'ORGANICITY_VERSION', '0.1.9' );
+define( 'ORGANICITY_VERSION', '0.1.10' );
 
 function get_id_by_slug($page_slug) {
     $page = get_page_by_path($page_slug);
